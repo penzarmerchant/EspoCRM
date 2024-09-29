43a497f261cbc1c371665cd81cb079c2d07fd7e5
penzar33 <penzarmerchant28@gmail.com>
2024-09-29 20:01:40 +0530

    Updated  CreateAccount Test file

--- tests/pages/accountPage.ts
+++ tests/pages/accountPage.ts
@@ -1,21 +1,32 @@
 import { Page, Locator } from "@playwright/test";
 import BasePage from "./basepage";
-import { L } from "@faker-js/faker/dist/airline-C5Qwd7_q";
+import { CreateAccount } from "./createAccount";
+import { AssignedUser } from "./assignedUser";
+import { Teams } from "./teams";
+import * as espoCRM from "@testData/espoCRM.json";
+import { generateMockData } from "@testData/generateTestData";
+import fs from 'fs/promises';
 
 export class AccountPage extends BasePage {
+  private readonly createAccount: CreateAccount;
+  private readonly assignedUser: AssignedUser;
+  private readonly teams: Teams;
   private readonly createAccountButton: Locator;
   private readonly accountNameTitle: Locator;
-  private readonly searchBarInputBox:Locator;
-  private readonly searchIcon:Locator;
-  private readonly searchResultAccountName:Locator;
+  private readonly searchBarInputBox: Locator;
+  private readonly searchIcon: Locator;
+  private readonly searchResultAccountName: Locator;
 
   constructor(page: Page) {
     super(page);
+    this.createAccount = new CreateAccount(page);
+    this.assignedUser=new AssignedUser(page);
+    this.teams=new Teams(page)
     this.createAccountButton = page.locator('a[title="Ctrl+Space"]');
     this.accountNameTitle = page.locator(".title");
-    this.searchBarInputBox=page.locator('input[data-name="textFilter"]');
-    this.searchIcon=page.locator('button[title="Search"]');
-    this.searchResultAccountName=page.locator('td[data-name="name"]');
+    this.searchBarInputBox = page.locator('input[data-name="textFilter"]');
+    this.searchIcon = page.locator('button[title="Search"]');
+    this.searchResultAccountName = page.locator('td[data-name="name"]');
   }
 
   async clickCreateAccountButton() {
@@ -26,19 +37,49 @@ export class AccountPage extends BasePage {
     return await this.getElementText(this.accountNameTitle);
   }
 
-  async enterNameOfAccount(nameofAccount:string){
-    await this.fillField(this.searchBarInputBox,nameofAccount);
+  async enterNameOfAccount(nameofAccount: string) {
+    await this.fillField(this.searchBarInputBox, nameofAccount);
   }
 
-  async clickSearchIcon(){
+  async clickSearchIcon() {
     await this.clickelement(this.searchIcon);
   }
 
-  async getSearchResultCount():Promise<number>{
+  async getSearchResultCount(): Promise<number> {
     return await this.getElementCount(this.searchResultAccountName);
   }
 
-  async getSearchResultAccountName():Promise<string>{
-   return await this.getElementText(this.searchResultAccountName)
+  async getSearchResultAccountName(): Promise<string> {
+    return await this.getElementText(this.searchResultAccountName);
+  }
+
+  async createCompleteAccount() {
+    await generateMockData("testData/espoCRM.json");
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    const updatedJson = await fs.readFile("testData/espoCRM.json", "utf-8");
+    await this.createAccount.enterName(espoCRM.nameofAccount);
+    await this.createAccount.enterWebsite(espoCRM.website);
+    await this.createAccount.enterEmail(espoCRM.email1);
+    await this.createAccount.enterPhone(espoCRM.phoneAccount);
+    await this.createAccount.enterBillingAddressStreet(
+      espoCRM.streetBillingAddress
+    );
+    await this.createAccount.enterBillingAddressCity();
+    await this.createAccount.enterBillingCity();
+    await this.createAccount.enterbillingAddressCounty(espoCRM.county1);
+    await this.createAccount.enterbillingPostalCode(espoCRM.postalCode1);
+    await this.createAccount.enterbillingAddressCountry(espoCRM.country1);
+    await this.createAccount.clickCopyButton();
+    await this.createAccount.enterAssignedUser();
+    await this.assignedUser.clickAssignedUserType();
+    await this.createAccount.enterTeams();
+    await this.teams.selectTeams();
+    await this.teams.clickSelect();
+    await this.createAccount.enterTypeofAccount();
+    await this.createAccount.selectAccountType();
+    await this.createAccount.clickIndustry();
+    await this.createAccount.selectIndustryType();
+    await this.createAccount.enterDescription(espoCRM.Description);
+    await this.createAccount.clickSave();
   }
 }
--- tests/spec/account_allscenarios.spec.ts
+++ tests/spec/account_allscenarios.spec.ts
@@ -1,10 +1,7 @@
     import { test} from '@fixtures/pomFixture';
     import { expect } from '@playwright/test';
     import * as espoCRM from '@testData/espoCRM.json';
-    import { generateMockData } from '@testData/generateTestData';
-    import fs from 'fs/promises';
 
-    var updatedEspoCRM;
     const nameErrorMessage='Name is required';
 
     test.describe.serial('Account Creation & Verification', ()=>{
@@ -14,42 +11,17 @@
         });
     
         test('Create New Account', async ({ homepage, accountPage, createAccount,assignedUser,teams }) => {
-            await generateMockData('testData/espoCRM.json');
-            await new Promise(resolve=> setTimeout(resolve,200));
-            const updatedJson= await fs.readFile('testData/espoCRM.json','utf-8');
-            updatedEspoCRM= JSON.parse(updatedJson);
             await homepage.clickaccountButton();
             await accountPage.clickCreateAccountButton();
-            await createAccount.enterName(updatedEspoCRM.nameofAccount);
-            await createAccount.enterWebsite(updatedEspoCRM.website);
-            await createAccount.enterEmail(updatedEspoCRM.email1);
-            await createAccount.enterPhone(updatedEspoCRM.phoneAccount);
-            await createAccount.enterBillingAddressStreet(updatedEspoCRM.streetBillingAddress)
-            await createAccount.enterBillingAddressCity();
-            await createAccount.enterBillingCity();
-            await createAccount.enterbillingAddressCounty(updatedEspoCRM.county1);
-            await createAccount.enterbillingPostalCode(updatedEspoCRM.postalCode1);
-            await createAccount.enterbillingAddressCountry(updatedEspoCRM.country1);
-            await createAccount.clickCopyButton();
-            await createAccount.enterAssignedUser();
-            await assignedUser.clickAssignedUserType();
-            await createAccount.enterTeams();
-            await teams.selectTeams();
-            await teams.clickSelect();
-            await createAccount.enterTypeofAccount();
-            await createAccount.selectAccountType();
-            await createAccount.clickIndustry();
-            await createAccount.selectIndustryType(); 
-            await createAccount.enterDescription(espoCRM.Description);
-            await createAccount.clickSave();
-            expect(await accountPage.getAccountTitleText()).toEqual(updatedEspoCRM.nameofAccount);
+            await accountPage.createCompleteAccount();
+            expect(await accountPage.getAccountTitleText()).toEqual(espoCRM.nameofAccount);
         });
     
         test('Search name of account as per the name created', async ({homepage,accountPage})=>{
                 await homepage.clickaccountButton();
-                await accountPage.enterNameOfAccount(updatedEspoCRM.nameofAccount);
+                await accountPage.enterNameOfAccount(espoCRM.nameofAccount);
                 await accountPage.clickSearchIcon();
-                expect(await accountPage.getSearchResultAccountName()).toEqual(updatedEspoCRM.nameofAccount);
+                expect(await accountPage.getSearchResultAccountName()).toEqual(espoCRM.nameofAccount);
                 expect(await accountPage.getSearchResultCount()).toEqual(1);
         })
     })
